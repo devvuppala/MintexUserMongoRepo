47daf7c202cefa104b19fa6dcdf8a1e04e8e252c
Dev Vuppala <dev.nandanv@gmail.com>
2019-04-10 20:36:34 -0400

    Test from eclipse

--- MintexUserMicroservice/src/main/java/com/mintex/UserService.java
+++ MintexUserMicroservice/src/main/java/com/mintex/UserService.java
@@ -1,29 +1,11 @@
 package com.mintex;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
-@RestController
-@RequestMapping("/user1")
-@CrossOrigin
+@Service
 public class UserService {
 	
-	@GetMapping(path="/hello", produces = "application/json")
-	public String sayHello() {
-		return "{\"name\": \"i am a service, you can call me an API. Updated\"}";
-	}
 	
-	@GetMapping(path="/users", produces = "application/json")
-	public User getUsers() {
-		User user =  new User();
-		user.setFirstname("dev");
-		user.setLastname("vup");
-		user.setUsername("dev");
-		return user;
-	}
 
 
 }
